90bf7e0edaa83b15ed7efc63170658188e40c3b4
Crowdbotics <team@crowdbotics.com>
2023-08-19 05:27:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ant-to-rent-42941",
+  url: "https://ant-to-rent-42941.botics.co",
+  api: "https://ant-to-rent-42941.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
